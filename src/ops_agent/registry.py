883ed97b4caed0c42080bf10e117b4f62b18064a
Fatemeh <fatemeh.ra80@gmail.com>
2026-08-12 23:38:52 +0330

    feat: add function registry

---
+++ src/ops_agent/registry.py
@@ -0,0 +1,13 @@
+from .tools import (
+            get_low_stock,
+            get_sales_summary,
+            get_discount_query,
+            update_stock,
+        )
+
+TOOL_REGISTRY = {
+    "get_low_stock": get_low_stock,
+    "get_sales_summary": get_sales_summary,
+    "get_discount_query": get_discount_query,
+    "update_stock": update_stock,
+}
